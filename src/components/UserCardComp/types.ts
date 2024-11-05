7b8bdbddcc0fe12daab32f2c74137c2cac4e151a
SergeiVS <fam.smirnoff@gmail.com>
2024-11-05 22:07:29 +0100

    add MyPosts, UserPostCard

--- src/components/UserCardComp/types.ts
+++ src/components/UserCardComp/types.ts
@@ -1,6 +1,6 @@
 import { Post } from "../../pages/AllPosts/types";
-export interface UserCardProps{
-post:Post
-key:string
-onDelete:()=>void
+export interface UserCardProps {
+  post: Post;
+  key: string;
+  onDelete: (id: number) => void;
 }
